5b2f04f46dea7cd0f815c8286a5919f25387f1ed
Ivan Katanić <ivan.katanic@gmail.com>
2016-02-29 22:50:41 +0100

    binomial heap

--- heap.ml
+++ heap.ml
@@ -135,3 +135,121 @@ module WeightBiasedHeap (Element:ORDERED) : (HEAP with module Elem = Element) =
            if size c >= size d + size h1 then T (nsize, y, c, (merge d h1))
            else T (nsize, y, (merge d h1), c)
   end
+
+
+module BinomialHeap (Element:ORDERED) : (HEAP with module Elem = Element) =
+  struct
+    module Elem = Element
+                    
+    type tree = Node of int * Elem.t * tree list
+    type heap = tree list
+
+    let empty = []
+    let isEmpty = function
+      | [] -> true
+      | _ -> false
+
+    let rank (Node (r, _, _)) = r
+    let root (Node (_, x, _)) = x
+
+    let link (Node (r, x1, l1) as t1) (Node (_, x2, l2) as t2) =
+      if Elem.compare x1 x2 < 0 then
+        Node (r + 1, x1, t2::l1)
+      else
+        Node (r + 1, x2, t1::l2)
+
+    let rec insTree t h =
+      match h with
+      | [] -> [t]
+      | hd::tl ->
+         if rank t < rank hd then t::h
+         else insTree (link hd t) tl
+
+    let insert x = insTree (Node (0, x, []))
+
+    let rec merge h1 h2 =
+      match h1, h2 with
+      | [], _ -> h2
+      | _, [] -> h1
+      | hd1::tl1, hd2::tl2 ->
+         if rank hd1 < rank hd2 then hd1 :: (merge tl1 h2)
+         else if rank hd2 < rank hd1 then hd2 :: (merge h1 tl2)
+         else insTree (link hd1 hd2) (merge tl1 tl2)
+
+    let rec removeMinTree = function
+      | [] -> raise Empty
+      | [hd] -> (hd, [])
+      | hd::tl ->
+         let t', ts' = removeMinTree tl in
+         if root hd < root t' then (hd, tl) else (t', hd::ts')
+                
+    let findMin h = 
+      let t, _ = removeMinTree h in root t
+
+    let deleteMin h =
+      let Node (_, _, ts'), ts = removeMinTree h in
+      merge (List.rev ts') ts
+
+    (* Exercise 3.5 *)
+    let rec findMin2 = function
+      | [] -> raise Empty
+      | [Node (_, x, _)] -> x
+      | Node (_, x, _) :: tl ->
+         let y = findMin2 tl in
+         if Elem.compare x y < 0 then x else y
+  end
+
+
+module BinomialHeap2 (Element:ORDERED) : (HEAP with module Elem = Element) =
+  struct
+    module Elem = Element
+                    
+    type tree = Node of Elem.t * tree list
+    type heap = (int * tree) list
+
+    let empty = []
+    let isEmpty = function
+      | [] -> true
+      | _ -> false
+
+    let root (Node (x, _)) = x
+
+    let link (Node (x1, l1) as t1) (Node (x2, l2) as t2) =
+      if Elem.compare x1 x2 < 0 then
+        Node (x1, t2::l1)
+      else
+        Node (x2, t1::l2)
+
+    let rec insTree r t h =
+      match h with
+      | [] -> [(r, t)]
+      | (hdr, hd)::tl ->
+         if r < hdr then (r, t)::h
+         else insTree (r+1) (link hd t) tl
+
+    let insert x = insTree 0 (Node (x, []))
+
+    let rec merge h1 h2 =
+      match h1, h2 with
+      | [], _ -> h2
+      | _, [] -> h1
+      | (r1, t1)::tl1, (r2, t2)::tl2 ->
+         if r1 < r2 then (r1, t1) :: (merge tl1 h2)
+         else if r2 < r1 then (r2, t2) :: (merge h1 tl2)
+         else insTree (r1+1) (link t1 t2) (merge tl1 tl2)
+
+    let rec removeMinTree = function
+      | [] -> raise Empty
+      | [(r, t)] -> (t, [])
+      | (r, t)::tl ->
+         let t', ts' = removeMinTree tl in
+         if root t < root t' then (t, tl) else (t', (r, t)::ts')
+                
+    let findMin h = 
+      let t, _ = removeMinTree h in root t
+
+    let deleteMin h =
+      let Node (_, ts'), ts = removeMinTree h in
+      let nt = List.mapi (fun i t -> (i, t)) (List.rev ts') in
+      merge nt ts
+  end
